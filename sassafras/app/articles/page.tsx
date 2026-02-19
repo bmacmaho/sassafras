70e089516022283dd52eb8fcab492b9d6dab76c8
BarraGit <macmahonbarra+git@gmail.com>
2026-02-19 15:28:15 +0100

    first commit

---
+++ sassafras/app/articles/page.tsx
@@ -0,0 +1,12 @@
+const ArticlesPage = () => {
+    return (
+        <div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-3xl font-bold">Articles Page</h1>
+            <p className="mt-4 text-lg">This is the articles page for the Sassafras app.</p>
+        </main>
+        </div>
+    );
+}
+
+export default ArticlesPage;
